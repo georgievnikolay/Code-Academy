bbd350c78b34f2fb92796f3b8077e785e0c75595
Nikolay Georgiev <32788663+georgievnikolay@users.noreply.github.com>
2021-01-14 13:52:37 +0200

    Add files via upload

---
+++ 20130113/20210113_3.c
@@ -0,0 +1,13 @@
+//Съберете signed char a = -10 с unsigned char z = 0 тип.
+
+#include<stdio.h>
+
+int main() {
+
+    char a = -10;
+    unsigned char z = 0;
+    
+
+    printf("%d\n", a + z); 
+    return 0;
+}
